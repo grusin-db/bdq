fbee589eac8c5b1d49ab3fe93500d6e40aeb259d
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2023-06-16 13:50:38 +0200

    new vscode pluggin

--- bdq/__init__.py
+++ bdq/__init__.py
@@ -1,10 +1,10 @@
 __version__ = "0.0.1"
 
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate()
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.getOrCreate()
 
-from pyspark.dbutils import DBUtils
-dbutils = DBUtils(spark)
+# from pyspark.dbutils import DBUtils
+# dbutils = DBUtils(spark)
 
 from .schema import compare_schemas
 from .dataframe import compare_dataframes, uncache_compare_dataframes_results, display_compare_dataframes_results
