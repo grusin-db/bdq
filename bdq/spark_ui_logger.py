f8ff0928ca562d79f5f7121b47c498cb904669de
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2023-07-12 09:04:06 +0200

    initial codebase

--- bdq/spark_ui_logger.py
+++ bdq/spark_ui_logger.py
@@ -3,37 +3,56 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
 class SparkUILogger:
-  stack:list[str] = []
+  _stack:list[str] = []
   
-  def __init__(self, desc, sc:SparkContext=None):  
-    self.sc = sc
-    self.desc = desc
+  def __init__(self, desc, spark:SparkSession=None, verbose=False):  
+    self._spark = spark or SparkSession.getActiveSession()
+    
+    if not self._spark:
+      raise ValueError("could not get active spark session")
 
-    if not self.sc:
-      spark = SparkSession.getActiveSession()
-      if not spark:
-        raise ValueError("could not get active spark session")
-      self.sc = spark.sparkContext
+    self._sc = self._spark.sparkContext
+    self._desc = desc
+    self._verbose = verbose
+
+  def set_job_description(self, desc):
+    self._sc.setLocalProperty(
+      "spark.job.description",
+      desc
+    )
 
   def __enter__(self):
-    #print(f">>> {self.desc}: {self.stack}")
-    self.sc.setJobDescription(self.desc)
-    self.stack.append(self.desc)
+    if self._verbose:
+      print(f">>> {self._desc}: {self._stack}")
+
+    self.set_job_description(self._desc)
+    self._stack.append(self._desc)
 
   def __exit__(self, exc_type, exc_value, traceback):
-    assert self.stack.pop() == self.desc
-    desc = self.stack[-1] if self.stack else None
-    self.sc.setJobDescription(desc)
-    #print(f"<<< {self.desc} -> {desc}")
+    assert self._stack.pop() == self._desc
+    desc = self._stack[-1] if self._stack else None
+    self.set_job_description(desc)
+    if self._verbose:
+      print(f"<<< {self._desc} -> {desc}")
 
   @staticmethod
-  def tag(function=None, desc=None):
+  def _is_spark_pinned_threads_supported(spark: SparkSession=None) -> bool:
+    try:
+      from py4j.clientserver import ClientServer
+      spark = spark or SparkSession.getActiveSession()
+      
+      return isinstance(spark.sparkContext._gateway, ClientServer)
+    except:
+      return False
+
+  @staticmethod
+  def tag(function=None, desc=None, verbose=False):
     def actual_decorator(f):
       @functools.wraps(f)
       def wrapper(*args, **kwargs):
         d = desc if desc else f.__qualname__ 
 
-        with SparkUILogger(d):
+        with SparkUILogger(d, verbose=verbose):  
           return f(*args, **kwargs)
       return wrapper
     if function:
